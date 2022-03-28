7a1bbe4e0f1d9e0f6f524957e6fd2ae4d8d10c6d
nazarko94 <71716013+nazarko94@users.noreply.github.com>
2022-03-28 20:59:38 +0300

    commit

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -40,63 +40,6 @@ export const AuthPage = () => {
     } catch (e) {}
   };
 
-  // return (
-  //   <div className="row">
-  //     <div className="col s6 offset-s3">
-  //       <h1>Short Link</h1>
-  //       <div className="card blue darken-1">
-  //         <div className="card-content white-text">
-  //           <span className="card-title">Authorization</span>
-  //           <div>
-  //             <div className="input-field">
-  //               <input
-  //                 placeholder="Введите email"
-  //                 id="email"
-  //                 type="text"
-  //                 name="email"
-  //                 className="yellow-input"
-  //                 value={form.email}
-  //                 onChange={changeHandler}
-  //               />
-  //               <label htmlFor="email">Email</label>
-  //             </div>
-
-  //             <div className="input-field">
-  //               <input
-  //                 placeholder="Введите пароль"
-  //                 id="password"
-  //                 type="password"
-  //                 name="password"
-  //                 className="yellow-input"
-  //                 value={form.password}
-  //                 onChange={changeHandler}
-  //               />
-  //               <label htmlFor="email">Password</label>
-  //             </div>
-  //           </div>
-  //         </div>
-  //         <div className="card-action">
-  //           <button
-  //             className="btn yellow darken-4"
-  //             style={{ marginRight: 10 }}
-  //             disabled={loading}
-  //             onClick={loginHandler}
-  //           >
-  //             Sign in
-  //           </button>
-  //           <button
-  //             className="btn grey lighten-1 black-text"
-  //             onClick={registerHandler}
-  //             disabled={loading}
-  //           >
-  //             Sign up
-  //           </button>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   return (
     <div className="login-form">
       <div className="form">
